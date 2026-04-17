d27ae114d1604ea30b3d7025b60f56416678987e
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2026-04-17 13:45:12 -0300

    chore: configura tailwind via cdn e limpa arquivos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold mb-4 text-center">RPG Bestiary</h1>
+  </div>
 </template>
